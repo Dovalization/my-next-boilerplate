d28c79b428ff1ef4b8021e58128c1ef3ec1d60a1
Guilherme Doval <guilherme.doval@aquiris.com.br>
2021-11-07 21:38:23 -0300

    Fixes incorrect export in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ interface IHomeProps {
   title: string
 }
 
-export const Home = ({ title = 'Hello World!' }: IHomeProps) => {
+const Home = ({ title = 'Hello World!' }: IHomeProps) => {
   return (
     <div>
       <h1>{title}</h1>
     </div>
   )
 }
+
+export default Home
